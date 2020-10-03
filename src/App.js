7975d5f7f7fb43c5a40dba501aa211b38d377721
fer <ferp@protonmail.ch>
2020-10-03 12:53:11 +0200

    line graph with a single path

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
-import { select } from "d3";
+import { select, line } from "d3";
 
-const initialData = [25, 45, 30, 74, 38];
+const initialData = [25, 45, 30, 74, 38, 80, 160, 240, 200, 100];
 
 function App() {
   const [data, setData] = useState(initialData)
   const svgRef = useRef();
   useEffect(() => {
     const svg = select(svgRef.current);
+    const myLine = line()
+      .x( (value, idx) => idx * 50)
+      .y( value => value);
+    // svg
+    //   .selectAll("circle")
+    //   .data(data)
+    //   .join("circle")
+    //   .attr("r", value => value)
+    //   .attr("cx", value => value * 2)
+    //   .attr("cy", value => value * 2)
+    //   .attr("stroke", "red")
     svg
-      .selectAll("circle")
-      .data(data)
-      .join("circle")
-      .attr("r", value => value)
-      .attr("cx", value => value * 2)
-      .attr("cy", value => value * 2)
-      .attr("stroke", "red")
-;
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", value => myLine(value))
+      .attr("fill", "none")
+      .attr("stroke", "blue")
   }, [data]);
   return <React.Fragment>
     <svg ref={svgRef}></svg>
